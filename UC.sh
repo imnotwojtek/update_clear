2f09dd5300eaa590ab1b873215340fa3ff8f2fbe
iiwaejwiqe <105372499+iiwaejwiqe@users.noreply.github.com>
2023-07-01 19:35:39 +0200

    Create UC.sh

---
+++ UC.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Aktualizacja systemu
+sudo apt update -y
+sudo apt upgrade -y
+sudo apt autoremove -y
+sudo apt clean
+sudo updatedb
+sudo systemctl restart systemd-journald
+
+# Optymalizacja systemu
+echo 'vm.swappiness=10' | sudo tee /etc/sysctl.conf
+echo 'vm.vfs_cache_pressure=50' | sudo tee -a /etc/sysctl.conf
+sudo sysctl -p
+
+# Wyczyszczenie cache serwera
+sudo sync && echo 3 | sudo tee /proc/sys/vm/drop_caches
+
+# Restart serwera
+sudo reboot
